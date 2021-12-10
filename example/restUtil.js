35eed5ca667db7f7031d52b2ecfb4acc5abb25c2
bytetang <jie@celer.network>
2021-12-10 10:33:55 +0800

    optimize code

--- example/restUtil.js
+++ example/restUtil.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { ethers } from "ethers";
 import { base64 } from "ethers/lib/utils";
-import { WithdrawReq as WithdrawReqProto, WithdrawType } from "../tx-proto/sgn/cbridge/v1/tx_pb";
+import { WithdrawReq as WithdrawReqProto, WithdrawType } from "../ts-proto/sgn/cbridge/v1/tx_pb";
 import { WithdrawMethodType } from "../ts-proto/sgn/gateway/v1/gateway_pb";
 
-/* eslint-disable camelcase */
 
-export const requestRestRefund = async (transfer_id, estimated, signer) => {
+/* eslint-disable camelcase */
+export const refund = async (transfer_id, estimated, signer) => {
   const timestamp = Math.floor(Date.now() / 1000);
   const withdrawReqProto = new WithdrawReqProto();
   withdrawReqProto.setXferId(transfer_id);
@@ -22,12 +22,13 @@ export const requestRestRefund = async (transfer_id, estimated, signer) => {
   const bytes = ethers.utils.arrayify(sig);
   const req = {
     withdraw_req: base64.encode(withdrawReqProto.serializeBinary() || ""),
-    sig: base64.encode(bytes || ""), // 用 withdraw_req 签名后可用
+    sig: base64.encode(bytes || ""), 
     estimated_received_amt: estimated,
     method_type: WithdrawMethodType.WD_METHOD_TYPE_ALL_IN_ONE,
   };
   return axios
-    .post(`${process.env.REACT_APP_SERVER_URL}/v1/withdrawLiquidity`, {//替换域名
+    // heads up to replace REACT_APP_SERVER_URL 
+    .post(`${process.env.REACT_APP_SERVER_URL}/v1/withdrawLiquidity`, {
       ...req,
     })
     .then(res => {
